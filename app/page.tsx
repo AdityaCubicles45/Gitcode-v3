f2c3e1e1dcb7925a67e2291ed4b23bec24f71429
AdityaCubicles45 <67513926+AdityaCubicles45@users.noreply.github.com>
2024-08-25 15:47:45 +0530

    ui added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,61 @@
 "use client";
 
-import Image from "next/image";
+import { Video } from './components/Video';
+import { Container } from './components/Container';
+import { Benefits } from './components/Benefits';
+import { Cta } from './components/Cta';
+import { Faq } from './components/Faq';
 import { Header } from './components/Header';
+import { Hero } from './components/Hero';
+import { SectionTitle } from './components/SectionTitle';
+import { Testimonials } from './components/Testimonials';
+
+import { benefitOne, benefitTwo } from "./components/data";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#C3FDBF]">
-      <Header />
-    </main>
+    <Container>
+      <Hero />
+      <SectionTitle
+        preTitle="Nextly Benefits"
+        title=" Why should you use this landing page"
+      >
+        Nextly is a free landing page & marketing website template for startups
+        and indie projects. Its built with Next.js & TailwindCSS. And its
+        completely open-source.
+      </SectionTitle>
+
+      <Benefits data={benefitOne} />
+      <Benefits imgPos="right" data={benefitTwo} />
+
+      <SectionTitle
+        preTitle="Watch a video"
+        title="Learn how to fullfil your needs"
+      >
+        This section is to highlight a promo or demo video of your product.
+        Analysts says a landing page with video has 3% more conversion rate. So,
+        don&apos;t forget to add one. Just like this.
+      </SectionTitle>
+
+      <Video videoId="fZ0D0cnR88E" />
+
+      <SectionTitle
+        preTitle="Testimonials"
+        title="Here's what our customers said"
+      >
+        Testimonials is a great way to increase the brand trust and awareness.
+        Use this section to highlight your popular customers.
+      </SectionTitle>
+
+      <Testimonials />
+
+      <SectionTitle preTitle="FAQ" title="Frequently Asked Questions">
+        Answer your customers possible questions here, it will increase the
+        conversion rate as well as support or chat requests.
+      </SectionTitle>
+
+      <Faq />
+      <Cta />
+    </Container>
   );
 }
