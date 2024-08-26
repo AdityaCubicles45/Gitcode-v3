54b3013c8b98beaedb46b6a94c338651e7e53e07
Meet2054 <meetaparekh2003@gmail.com>
2024-08-26 13:16:46 +0530

    Navbar update

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,10 @@ import ThemeChanger from "./DarkSwitch";
 import Image from "next/image"
 import { Disclosure } from "@headlessui/react";
 import { Header } from "./Header";
+import { MenuIcon } from 'lucide-react'
+import { useWallet } from '@aptos-labs/wallet-adapter-react'
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+
 
 export const Navbar = () => {
   const navigation = [
@@ -13,15 +17,16 @@ export const Navbar = () => {
     "Company",
     "Blog",
   ];
+  const {account} = useWallet();
 
   return (
     <div className="w-full">
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+      <nav className="container relative flex flex-wrap items-center justify-between  p-8 mx-auto  xl:px-0">
         {/* Logo  */}
         <Disclosure>
           {({ open }) => (
             <>
-              <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
+              <div className="flex flex-wrap items-center justify-between w-full lg:w-auto ">
                 <Link href="/">
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
@@ -36,47 +41,31 @@ export const Navbar = () => {
                     <span>Gitcode-v3</span>
                   </span>
                 </Link>
-
-                <Disclosure.Button
-                  aria-label="Toggle Menu"
-                  className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
-                  <svg
-                    className="w-6 h-6 fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24">
-                    {open && (
-                      <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                      />
-                    )}
-                    {!open && (
-                      <path
-                        fillRule="evenodd"
-                        d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                      />
-                    )}
-                  </svg>
-                </Disclosure.Button>
-
-                <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  <>
-                    {navigation.map((item, index) => (
-                      <Link key={index} href="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
-                          {item}
-                      </Link>
-                    ))}        
-                  </>
-                </Disclosure.Panel>
+               
               </div>
             </>
           )}
         </Disclosure>
 
-        <Header className="hidden mr-4 space-x-4 lg:flex nav__item" />
-
+        {/* <Header /> */}
+          <div className="flex flex-row gap-4 " >
+          <aside className=" ">
+                <WalletSelector />
+                {account?.address &&  <Link
+                    href="/dashboard"
+                    className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 "
+                  >
+                    <span className="absolute  inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+                    <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ">
+                Dashboard
+                    </span>
+                  </Link>}
+                
+                  {/* {user ? <UserButton afterSignOutUrl="/" /> : null} */}
+                  <MenuIcon className="md:hidden" />
+                </aside>
           <ThemeChanger />
+          </div>
       </nav>
     </div>
   );
